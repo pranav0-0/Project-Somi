01e4cfb65f439b52e66af858ee7f2874b5910b50
Pranav Chavan <7pranav.chavan@gmail.com>
2021-11-26 13:56:08 +0530

    changes to aWord variable

--- content.js
+++ content.js
@@ -1,10 +1,10 @@
 window.onload = function get_body() {
 
     let content = document.getElementsByTagName('*')
-    let aWord = /shit| fuck| piss| bitch| ass/i;
+    let aWord = /shit|fuck|f*uck|piss|bitch|dick|cock|pussy|suck|anus|anul|asshole|bastard|blowjob|blow job|boob|cum|rape|handjob|hand job|incest|jerk off|jerk|masturbate|nigger|nude|slut|tits|titties|twat| ass/i;
     var count = 0;
     for (let i = 0; i < content.length; i++) {
-        content[i].innerHTML = content[i].innerHTML.replace(aWord, "*****",count++);
+        content[i].innerHTML = content[i].innerHTML.replace(aWord, "****");
     }
     console.log(count);
 }
