448b65548c0d922d70d408e2b0a114838bfe0ddb
Pranav Chavan <7pranav.chavan@gmail.com>
2021-11-26 15:01:19 +0530

    added bWord and verson change

--- content.js
+++ content.js
@@ -1,10 +1,12 @@
 window.onload = function get_body() {
 
     let content = document.getElementsByTagName('*')
-    let aWord = /shit|fuck|f*uck|piss|bitch|dick|cock|pussy|suck|anus|anul|asshole|bastard|blowjob|blow job|boob|cum|rape|handjob|hand job|incest|jerk off|jerk|masturbate|nigger|nude|slut|tits|titties|twat| ass/i;
+    let aWord = /shit|fuck|f*uck|piss|bitch|dick|cock|pussy|suck|anus|anul|asshole|bastard|blowjob|blow job|boob/i;
+    let bWord = /rape|handjob|hand job|incest|jerk off|jerk|masturbate|nigger|nude|slut|tits|titties|twat| ass| cum/i;
     var count = 0;
     for (let i = 0; i < content.length; i++) {
-        content[i].innerHTML = content[i].innerHTML.replace(aWord, "****");
+        content[i].innerHTML = content[i].innerHTML.replace(aWord,function(x){count+=1;return "****"});
+        content[i].innerHTML = content[i].innerHTML.replace(bWord,function(x){count+=1;return "****"});
     }
     console.log(count);
 }
