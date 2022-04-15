eb4ad82280388a00769a0b9c12647b4c51f95796
Pranav Chavan <7pranav.chavan@gmail.com>
2022-04-15 19:22:10 +0530

    Added menu Animation

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-document.querySelector('.wrapper').addEventListener('click', (e) => {
-    e.currentTarget.classList.toggle('is-active');
-  });
-  
 getCount();
 function getCount() {
     chrome.storage.local.get(function(result) {
         document.getElementById("number").innerHTML = result.test;
     })
 }
+
+// menu button
+document.querySelector('.wrapper').addEventListener('click', (e) => {
+    e.currentTarget.classList.toggle('is-active');
+  });
+  
