24d28d7b61f66470ca310718acccfd0b23cd3ac0
Pranav Chavan <7pranav.chavan@gmail.com>
2022-04-16 18:45:56 +0530

    Improved side menu navigation

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-getCount();
-function getCount() {
-    chrome.storage.local.get(function(result) {
-        document.getElementById("number").innerHTML = result.test;
-    })
-}
+// getCount();
+// function getCount() {
+//     chrome.storage.local.get(function(result) {
+//         document.getElementById("number").innerHTML = result.test;
+//     })
+// }
 
 // menu button
 document.querySelector('.wrapper').addEventListener('click', (e) => {
