40b6f6e4c88a127f89f0cff264bdb5a84008d2cd
Pranav Chavan <7pranav.chavan@gmail.com>
2022-04-27 20:25:31 +0530

    minor changes

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-// getCount();
-// function getCount() {
-//     chrome.storage.local.get(function(result) {
-//         document.getElementById("number").innerHTML = result.test;
-//     })
-// }
+getCount();
+function getCount() {
+    chrome.storage.local.get(function(result) {
+        document.getElementById("number").innerHTML = result.test;
+    })
+}
 
 // menu button
 document.querySelector('.wrapper').addEventListener('click', (e) => {
     e.currentTarget.classList.toggle('is-active');
   });
-  
