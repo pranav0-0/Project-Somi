b465a9247513d95e8353935155340b72d3b93d98
Pranav Chavan <7pranav.chavan@gmail.com>
2022-04-27 20:29:34 +0530

    Testing Database integration & manifest version v3

--- Test Extension/content.js
+++ Test Extension/content.js
@@ -1,40 +1,37 @@
-if(document.readyState=="complete"){
-    textReplace();
-}
-var count=0;
-
-chrome.storage.local.get(function(result) {
-    console.log('Get func\nValue is set to ');
-    console.log(result.test);
-
-})
-
-var conContent = chrome.runtime.connect({name: "connected-content"});
-var aWord = /toast/i;
-
-function textReplace() {
-    var selectedSpan = document.getElementsByTagName("span");
-    for (let i = 0; i < selectedSpan.length; i++) {
-        selectedSpan[i].innerHTML = selectedSpan[i].innerText.replace(aWord,function(x){count+=1;return " *** "});
-    }
-    conContent.postMessage({count: count});
-    console.log("No. Text replaced: " + count);
-}
-
-var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-
-const observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutation) {
-        if(mutation.addedNodes.length || mutation.removedNodes.length) {
-            textReplace();
+// if(document.readyState=="complete"){
+//     getFunc();
+// }
+getFunc()
+function getFunc() {
+    chrome.storage.local.get(function(result) {
+        var count = 0;
+        var content = document.getElementsByTagName("span");
+        aWord = result.test;
+        var reWord = new RegExp(aWord, "i");
+        console.log(aWord);
+        console.log(reWord);
+        for (let i = 0; i < content.length; i++) {
+            content[i].innerHTML = content[i].innerText.replace(reWord,function(x){count+=1;return " **** "});
         }
+        console.log("Text replaced: ");
+        console.log(count);
     })
-})
-if(document.readyState=="complete"){
-observer.disconnect();
 }
-else
-observer.observe(document.body, {
-    childList: true,
-    // subtree: true,
-});
+
+
+// var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+// const observer = new MutationObserver(function(mutations) {
+//     mutations.forEach(function(mutation) {
+//         if(mutation.addedNodes.length || mutation.removedNodes.length) {
+//             getFunc();
+//         }
+//     })
+// })
+// if(document.readyState=="complete"){
+// observer.disconnect();
+// }
+// else
+// observer.observe(document.body, {
+//     childList: true,
+//     // subtree: true,
+// });
