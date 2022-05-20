206b5e179bcfd357999967001172c7c591e6e69f
Pranav Chavan <7pranav.chavan@gmail.com>
2022-05-20 20:50:24 +0530

    Implemented database

--- content.js
+++ content.js
@@ -8,41 +8,41 @@ var linkFlag = false;
 var conContent = chrome.runtime.connect({name: "connected-content"});
 
 function textReplace(){
-    let aWord = /shit|fuck|piss|bitch|dick|cock|pussy|suck|anus|anul|asshole|bastard|blowjob|blow job|boob/i;
-    let bWord = /rape|handjob|hand job|incest|jerk off|jerk|masturbate|nigger|nude|slut|tits|titties|twat| cum/i;
-    var content = document.getElementsByTagName("span");
-
-    if(window.location.href.includes('google.com/search?'))
-    googleSearchLink(aWord,bWord,content);
-    else {
-        for (let i = 0; i < content.length; i++) {
-            content[i].innerHTML = content[i].innerText.replace(aWord,function(x){count+=1;return " **** "});
-            content[i].innerHTML = content[i].innerText.replace(bWord,function(x){count+=1;return " **** "});
+    chrome.storage.local.get(function(allWords) {
+        var content = document.getElementsByTagName("span");
+        var nonRegexWord = allWords.word;
+        var aWord = new RegExp(nonRegexWord, "i");
+        if(window.location.href.includes('google.com/search?'))
+        googleSearchLink(aWord,content);
+        else {
+            for (let i = 0; i < content.length; i++) {
+                content[i].innerHTML = content[i].innerText.replace(aWord,function(x){count+=1;return " **** "});
+            }
         }
-    }
-    console.clear();
-    console.log("Text replaced: ");
-    console.log(count);
-    conContent.postMessage({count: count});
-    if(count>100 && countFlag == false){
-        countFlag = true;
-        alert("This webpage contains sensitive content. It is recommended to leave this site.");
-    }
+        console.clear();
+        console.log("Text replaced: ");
+        console.log(count);
+        conContent.postMessage({count: count});
+        if(count>100 && countFlag == false){
+            countFlag = true;
+            alert("This webpage contains sensitive content. It is recommended to leave this site.");
+        }
+    })
 }
 
-function googleSearchLink(aWord,bWord,content) {
+function googleSearchLink(aWord,content) {
     var h3Content =  document.getElementsByTagName("h3");
     try {
+        console.log("tried");
         for (let i = 0; i < content.length; i++) {
             content[i].innerHTML = content[i].innerHTML.replace(aWord,function(x){count+=1;return " **** "});
-            content[i].innerHTML = content[i].innerHTML.replace(bWord,function(x){count+=1;return " **** "});
             h3Content[i].innerHTML = h3Content[i].innerHTML.replace(aWord,function(x){count+=1;return " **** "});
-            h3Content[i].innerHTML = h3Content[i].innerText.replace(bWord,function(x){count+=1;return " **** "});
+            image.png
         }
     } catch (TypeError) {
         for (let i = 0; i < content.length; i++) {
             content[i].innerHTML = content[i].innerHTML.replace(aWord,function(x){count+=1;return " **** "});
-            content[i].innerHTML = content[i].innerHTML.replace(bWord,function(x){count+=1;return " **** "});
+            // content[i].innerHTML = content[i].innerHTML.replace(bWord,function(x){count+=1;return " **** "});
         }
     }
 }
